464f6faead71ac92968672ca5761cb6213f114e4
Li Zhaoyang <lizy14@yeah.net>
2016-12-21 01:35:59 +0800

    两个计数器，两位数码管显示，分别由定时器、按键控制

--- 51/main.asm
+++ 51/main.asm
@@ -1,7 +1,12 @@
 
-; 单个计数器，数码管一位显示，中断清零
+; 两个计数器，两位数码管显示，分别由定时器、按键控制
 ; lizy14@mails.tsinghua.edu.cn
-; 2016-12-16
+; 2016-12-21
+
+BUTTONDOWN EQU 05H ; time till the next button press to be able to be responded
+COUNTERX EQU 00H   ; low digit controlled by timer
+COUNTERY EQU 01H   ; digit controlled by button
+COUNTERT EQU 03H   ; time since last increase of timer counter
 
 ORG  0000H
     LJMP MAIN
@@ -9,54 +14,128 @@ ORG  0000H
 ORG 0003H
     LJMP EXINT
 
-EXINT:  ; external interrupt handler
+ORG 000BH
+    LJMP TMINT
+
+ORG 0013H
+    LJMP EXINT
+
+
+TMINT:
+    ; timer overflow interrupt handler
+    ; increse counter X
     PUSH PSW
-    CALL RESET
+    PUSH ACC
+
+    MOV A, BUTTONDOWN
+    CJNE A, #00H, DODEC
+        DONTDEC:
+        SJMP DONEDEC
+        DODEC:
+        DEC A
+        DONEDEC:
+    MOV BUTTONDOWN, A
+
+    MOV A, COUNTERT
+    INC A
+    CJNE A, #020H, DONOTINCRESE
+        DOINCREASE:
+        MOV A, COUNTERX
+        INC A
+        CJNE A, #0AH, NOCARRYX
+            CARRYX:
+            MOV A, #0H
+            NOCARRYX:
+        MOV COUNTERX, A
+        DONOTINCRESE:
+    MOV COUNTERT, A
+
+    POP ACC
     POP PSW
     RETI
 
-INIT:  ; enable external interrupt
+EXINT:
+    ; external interrupt handler
+    ; increase counter Y
+    PUSH PSW
+    PUSH ACC
+    CLR EA
+    MOV A, BUTTONDOWN
+    CJNE A, #0H, DONOTINCREASEY
+        DOINCREASEY:
+        MOV A, #010H
+        MOV BUTTONDOWN, A
+        MOV A, COUNTERY
+        INC A
+        CJNE A, #0AH, NOCARRYY
+            CARRYY:
+            MOV A, #0
+            NOCARRYY:
+        MOV COUNTERY, A
+        DONOTINCREASEY:
+
     SETB EA
+    POP ACC
+    POP PSW
+    RETI
+
+INIT:
+
+    ; set up external interrupt
     SETB EX0
-    CLR  IT0
+    SETB IT0
+    SETB EX1
+    SETB IT1
+
+    ; set up timer interrupt
+    SETB ET0
+    MOV TH0, #0FFH
+    MOV TL0, #0FFH
+    SETB TR0
+
+    SETB EA ; enable interrupt
+
+    MOV DPTR, #DIGITS_TABLE  ; digit translation
+    MOV SP, #30H ; set up stack pointer
+
+    MOV COUNTERX, #2H
+    MOV COUNTERY, #4H
+    MOV COUNTERT, #0H
+    MOV BUTTONDOWN, #0H
+
     RET
 
-DRAW:  ; display value of B
-    MOV  A, B
+DRAW:  ; display counters
+    MOV A, COUNTERX
     MOVC A, @A+DPTR
-    MOV  P0, A
+    MOV P0, A
+
+    CLR P2.3
+    CALL SMALLDELAY
+    SETB P2.3
+
+    MOV A, COUNTERY
+    MOVC A, @A+DPTR
+    MOV P0, A
+
+    CLR P2.2
+    CALL SMALLDELAY
+    SETB P2.2
+
     RET
 
-RESET:  ; reset value of B
-    MOV  B, #0
-    CALL DRAW
+
+SMALLDELAY:
+    MOV R7, #0FFH
+    D0: DJNZ R7, D0
     RET
+
 
 MAIN:
     CALL INIT
-    MOV  B, #0
-    MOV  P2, #07h    ; display mask
-    MOV  DPTR, #DIGITS_TABLE
-
-
-LOOP_BEGIN:
-
-    CALL DRAW
-
-    INC  B
-    ANL  B, #15
-    CALL DELAY
-
+    LOOP_BEGIN: CALL DRAW
     SJMP LOOP_BEGIN
 
-DELAY:
-    MOV R5, #20
-    D1: MOV R6, #20
-    D2: MOV R7, #248
-    DJNZ R7, $
-    DJNZ R6, D2
-    DJNZ R5, D1
-    RET
 
 DIGITS_TABLE:
     DB 0C0H,0F9H,0A4H,0B0H,099H,092H,082H,0F8H
